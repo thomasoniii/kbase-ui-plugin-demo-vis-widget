f2c17867cee9c92df94eed19420a3504591b5317
Jim Thomason <jim@japanesewartuba.com>
2015-11-05 19:33:15 -0600

    circular heatmap

---
+++ src/plugin/modules/circularheatmap.js
@@ -0,0 +1,126 @@
+/*global
+ define
+ */
+/*jslint
+ browser: true,
+ white: true
+ */
+define([
+    'bluebird',
+    'jquery',
+    'underscore',
+    'kb_common_dom',
+    'kb_vis_circular_heatmap'],
+    function (Promise, $, _, dom) {
+        'use strict';
+        function widget(config) {
+            var mount, container, runtime = config.runtime;
+            function render() {
+
+                var generate_hm_data = function generate_hm_data() {
+
+                    var newDataset = [];
+                    for (var i = 0; i < numPies; i++) {
+                        newDataset[i] = [];
+                    }
+                    for (var j = 0; j < newDataset.length; j++) {
+                        for (var i = 0; i < numSlices; i++) {
+
+                            var value = Math.random();
+
+                            newDataset[j].push(
+                                {
+                                    val : value,
+                                    label : 'Label ' + (i + 1),
+                                    tooltip : 'Ring : ' + (j + 1) + '<br>' + 'Label : ' + (i + 1) + '<br>' + 'Value : ' + value,
+                                }
+                            );
+                        }
+                    };
+
+                    return newDataset;
+                }
+
+                var hm_data = generate_hm_data();
+
+                var $hm = $.jqElem('div')
+                    .css({width: '800px', height: '500px'})
+                    .kbaseCircularHeatmap({
+                        labels : false,
+                        innerRadius : 50,
+                        startingPosition : 'top',
+                        childOptions : {
+                            labels : false,
+                            startingPosition : 'top'
+                        },
+                        dataset: hm_data
+                    });
+
+                var $demo = $.jqElem('div')
+                    .append($hm.$elem)
+                    .append(
+                        $.jqElem('button')
+                            .on('click', function(e) {
+                                $hm.setDataset(generate_hm_data());
+                            })
+                            .append('Randomize heatmap')
+                    )
+                ;
+
+                return {
+                    title: 'Sample heatmap',
+                    content: $demo
+                };
+            }
+
+            function init(config) {
+                return new Promise(function (resolve) {
+                    resolve();
+                });
+            }
+
+            function attach(node) {
+                return new Promise(function (resolve) {
+                    mount = node;
+                    container = dom.createElement('div');
+                    mount.appendChild(container);
+                    var rendered = render();
+
+                    runtime.send('ui', 'setTitle', rendered.title);
+                    $(container).append(rendered.content);
+
+                    resolve();
+                });
+            }
+            function start(params) {
+                return new Promise(function (resolve) {
+                    resolve();
+                });
+            }
+            function stop() {
+                return new Promise(function (resolve) {
+                    resolve();
+                });
+            }
+            function detach() {
+                return new Promise(function (resolve) {
+                    $(container).empty();
+                    resolve();
+                });
+            }
+
+            return {
+                init: init,
+                attach: attach,
+                start: start,
+                stop: stop,
+                detach: detach
+            };
+        }
+
+        return {
+            make: function (config) {
+                return widget(config);
+            }
+        };
+    });
